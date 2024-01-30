0cf1d498f6e03162bbf1f76766f523e76c1469af
andrew <andrewliu278@gmail.com>
2024-01-30 17:36:18 -0500

    modularize Log

---
+++ src/lib/components/Log.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import type { Log } from "$lib/types";
+  export let log: Log;
+</script>
+
+<div class="mb-8">
+  <span class="text-xs text-slate-700">{log.date}</span>
+  <span class="text-xl text-brightred">#</span>
+  <span>
+    <a
+      href={`logs/${log.slug}`}
+      class="text-xl hover:text-brightred transition-colors duration-75"
+      >{log.title}</a
+    >
+  </span>
+  <div class="mt-2 text-md text-slate-700">{log.subtitle}</div>
+</div>
--- src/routes/logs/+page.svelte
+++ src/routes/logs/+page.svelte
@@ -1,14 +1,8 @@
 <script lang="ts">
+  import Log from "$lib/components/Log.svelte";
   export let data;
 </script>
 
 {#each data.logs as log}
-  <div class="post">
-    <span class="post-date">{log.date}</span>
-    <span class="post-pound">#</span>
-    <span class="post-title">
-      <a href={`logs/${log.slug}`}>{log.title}</a>
-    </span>
-    <div class="post-subtitle">{log.subtitle}</div>
-  </div>
+  <Log {log} />
 {/each}
